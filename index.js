f26e2e19fc5097c3cfef7d9ab6a284e1e639b209
Junior Schmidt <junior.schmidt.99@gmail.com>
2022-05-28 13:55:37 -0300

    Backend validation

--- index.js
+++ index.js
@@ -23,6 +23,34 @@ app.get('/',(req, res)=> {
     res.render('index');
 })
 
+app.post('/form',(req,res)=>{
+    var { email, name, cpf} = req.body;
+
+    var emailError;
+    var nameError;
+    var cpfError;
+
+    if (email == undefined || email ==''){
+        emailError = 'EMAIL is not valid';
+    }
+
+    if(name == undefined || name =='' || name.length < 3 ){
+        nameError = 'NAME is not valid';
+    }
+
+    if (cpf == undefined || cpf.length < 11 ){
+        cpfError = 'CPF is not valid';
+    }
+
+    if(emailError!= undefined || nameError!= undefined || cpfError != undefined){
+        res.send('Check the fields typed correctly')
+        res.redirect('/')
+    }else{
+        res.send('Submitted form');
+        res.redirect('/')
+    }
+})
+
 app.listen(8080,(req,res)=>{
     console.log('Servidor Rondando!');
 })
